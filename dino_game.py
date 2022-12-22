eedd75adf80b24abd9b0ad8aff2280ea04f4989c
kliprmimo <grzes.dziechciarz@gmail.com>
2022-12-22 12:42:43 +0100

    update

--- dino_game.py
+++ dino_game.py
@@ -10,17 +10,31 @@ background_graveyard_2 = background_graveyard_1
 ground = pygame.image.load('graphics/ground.png').convert_alpha()   # 1387x180
 grave = pygame.image.load('graphics/grave.png').convert_alpha()    # 800x840
 grave = pygame.transform.scale(grave, (100, 105))
+grave_2 = pygame.image.load('graphics/grave.png').convert_alpha()    # 800x840
+grave_2 = pygame.transform.scale(grave_2, (100, 105))
 background = pygame.Surface((1280, 720))
 background.fill('grey')
 game_clock = pygame.time.Clock()
 
-background_x_coord = 0
-ground_x_coord = 0
-game_speed = 6
-jump = 0
+background_x_coord = 0  # always 0
+ground_x_coord = 0  # always 0
+game_speed = 12
+
+grave_x_coord = 1300
+grave_y_coord = 650
+grave_2_x_coord = 2000
+grave_2_y_coord = 650
+
+#   jump setup
+
+jump = 0  # always 0
 player_x_coord = 650
-player_x_coord_change = 24
-i = 0
+jump_count_down = 0  # always 0
+jump_len = 40   # jump length in frames
+player_x_coord_change_base = 30  # changes speed of jump
+player_x_coord_change = player_x_coord_change_base
+player_acceleration = 2*player_x_coord_change_base/jump_len
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -42,7 +56,13 @@ while True:
     ground_2_rect = ground.get_rect(bottomleft=(ground_x_coord+1387, 770))
     screen.blit(ground, ground_2_rect)
 
-    screen.blit(grave, (0, 0))
+    grave_rect = grave.get_rect(bottomleft=(grave_x_coord, grave_y_coord))
+    screen.blit(grave, grave_rect)
+    pygame.draw.circle(grave, (255, 255, 255), (grave_x_coord + 55, grave_y_coord - 60), 45)
+
+    grave_2_rect = grave_2.get_rect(bottomleft=(grave_2_x_coord, grave_2_y_coord))
+    screen.blit(grave_2, grave_2_rect)
+    pygame.draw.circle(grave_2, (255, 255, 255), (grave_2_x_coord + 55, grave_2_y_coord - 60), 45)
 
     character_rect = character.get_rect(bottomleft=(20, player_x_coord))
     screen.blit(character, character_rect)
@@ -50,6 +70,8 @@ while True:
     # move of graves and tree background
     background_x_coord -= game_speed
     ground_x_coord -= game_speed
+    grave_x_coord -= game_speed
+    grave_2_x_coord -= game_speed
 
     if background_x_coord < -1863:
         background_x_coord = 0
@@ -61,21 +83,21 @@ while True:
         jump = 1
 
     if jump:
-        if i < 30:
-            i += 1
-            player_x_coord_change -= 0.8
+        if jump_count_down < jump_len/2:
+            jump_count_down += 1
+            player_x_coord_change -= player_acceleration
             player_x_coord -= player_x_coord_change
-        if i == 30:
+        if jump_count_down == jump_len/2:
             player_x_coord_change = 0
-            i += 1
-        if 30 < i < 60:
-            i += 1
-            player_x_coord_change += 0.8
+            jump_count_down += 1
+        if jump_len/2 < jump_count_down < jump_len:
+            jump_count_down += 1
+            player_x_coord_change += player_acceleration
             player_x_coord += player_x_coord_change
-        if i == 60:
+        if jump_count_down == jump_len:
             jump = 0
-            i = 0
-            player_x_coord_change = 24
+            jump_count_down = 0
+            player_x_coord_change = player_x_coord_change_base
 
     pygame.display.update()
     game_clock.tick(60)
